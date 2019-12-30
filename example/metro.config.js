5aafedb5a8833d8c745663d3fa1463aa7a6875dd
Thomas <bbkings2002@gmail.com>
2019-12-30 13:51:16 +0800

    支持native-module hot load

--- example/metro.config.js
+++ example/metro.config.js
@@ -5,7 +5,21 @@
  * @format
  */
 
+const path = require('path');
+const blacklist = require('metro-config/src/defaults/blacklist');
+
+const nativeModule = path.resolve(__dirname, '..');
+
 module.exports = {
+  watchFolders: [
+    path.resolve(__dirname, 'node_modules'),
+    nativeModule,
+  ],
+  resolver: {
+    blacklistRE: blacklist([
+      new RegExp(`${nativeModule}/node_modules/react-native/.*`),
+    ]),
+  },
   transformer: {
     getTransformOptions: async () => ({
       transform: {
